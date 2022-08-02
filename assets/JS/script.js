4dcec55313d9952b8671edd4983502ab2b8c3a26
luksouza <apolloboficial@gmail.com>
2022-08-02 11:49:07 -0300

    .

---
+++ assets/JS/script.js
@@ -0,0 +1,6 @@
+const buttonCreateCards = document.querySelector("#new-card");
+const modalCard = document.querySelector(".modal-card");
+
+const createNewCard = event => modalCard.classList.remove('disabled-modal')
+
+buttonCreateCards.addEventListener("click", createNewCard)
